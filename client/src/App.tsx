a2fadb6fba5e1a7b4cfaa82518b0636b1d8641ad
KamalasankariS <iamkamalamskls@gmail.com>
2026-07-25 16:16:30 -0400

    feat: mount toast container in app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,25 +6,34 @@ import { DeductionsPage } from './pages/DeductionsPage'
 import { DeductionDetailPage } from './pages/DeductionDetailPage'
 import { ImportAuditPage } from './pages/ImportAuditPage'
 import { AuthPage } from './pages/AuthPage'
+import { ToastContainer } from './components/Toast'
 import { getToken } from './api/client'
 
 export default function App() {
   const [authed, setAuthed] = useState(!!getToken())
 
   if (!authed) {
-    return <AuthPage onAuth={() => setAuthed(true)} />
+    return (
+      <>
+        <AuthPage onAuth={() => setAuthed(true)} />
+        <ToastContainer />
+      </>
+    )
   }
 
   return (
-    <AppShell>
-      <Routes>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/deductions" element={<DeductionsPage />} />
-        <Route path="/deductions/:id" element={<DeductionDetailPage />} />
-        <Route path="/import-audit" element={<ImportAuditPage />} />
-        <Route path="/login" element={<Navigate to="/" replace />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </AppShell>
+    <>
+      <AppShell>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/deductions" element={<DeductionsPage />} />
+          <Route path="/deductions/:id" element={<DeductionDetailPage />} />
+          <Route path="/import-audit" element={<ImportAuditPage />} />
+          <Route path="/login" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AppShell>
+      <ToastContainer />
+    </>
   )
 }
